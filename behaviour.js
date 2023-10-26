72b8168c60995662cdec0241a218f4e33c4cd0ee
gritNgo <dustspeck00@gmail.com>
2023-10-26 06:33:25 +0100

    Initial commit

---
+++ behaviour.js
@@ -0,0 +1,8 @@
+window.onload = () => {
+    const gridContainer = document.getElementById("grid-container");
+    const newDiv = document.createElement("div");
+
+    
+
+    gridContainer.appendChild(newDiv);
+}
