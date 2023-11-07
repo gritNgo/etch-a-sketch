8efed8e8b25645688243911eb18f6791c484bcc2
gritNgo <dustspeck00@gmail.com>
2023-11-07 15:25:54 +0000

    new grid button working but must fix overflows

--- app.js
+++ app.js
@@ -7,31 +7,50 @@ const newGridBtn = document.createElement("button");
 document.body.appendChild(newGridBtn).className = "newGridBtn";
 newGridBtn.textContent = "New grid";
 
-function getSize(newRows, newColumns) {
-    newRows = prompt(`Enter new number of rows: `);
-    newColumns = prompt(`Enter new number of columns: `);
-
-}
 newGridBtn.addEventListener("click", () => {
-    getSize(newRows, newColumns);
-    console.log(`${newRows}, ${newColumns}`);
+    let newRows = prompt(`Enter new number of rows: `);
+    let newCells = prompt(`Enter new number of cells: `);
+    container.innerHTML = '';
+    
+    function newGrid() {
+        createNewRows(newRows);
+        createNewCells(newCells);
+    }
+    function createNewRows(newRows) {
+        for (let r = 0; r < newRows; r++) {
+            let rows = document.createElement("div");
+            container.appendChild(rows).className = "gridRow";
+        }
+    }
+    function createNewCells(newCellNum) {
+        for (let i = 0; i < rows.length; i++) {
+            for (j = 0; j < newCellNum; j++) {
+                let newCell = document.createElement("div");
+                rows[j].appendChild(newCell).className = "cell";
+    
+                newCell.addEventListener("mouseover", () => {
+                    newCell.style.backgroundColor = "black"
+                });
+            }
+        }
+    }
+    newGrid();
+
 });
 
-
-
 function defaultGrid() {    
-    makeRows(16);
-    makeCells(16);
+    createRows(16);
+    createCells(16);
 }
 
-function makeRows(rowNum) {
+function createRows(rowNum) {
     for (let r = 0; r < rowNum; r++) {
         let rows = document.createElement("div");
         container.appendChild(rows).className = "gridRow";
     }
 }
 
-function makeCells(cellNum) {
+function createCells(cellNum) {
     for (let i = 0; i < rows.length; i++) {
         for (j = 0; j < cellNum; j++) {
             let newCell = document.createElement("div");
